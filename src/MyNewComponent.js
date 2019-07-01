e498246a99bd811f0b91f5ce250fab7c6c7db81b
Goutam Shepur <goutam.s.h@gmail.com>
2019-07-02 00:47:43 +0100

    With new compomenent

---
+++ src/MyNewComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyNewComponent extends React.Component {
+	render(){
+		return (
+			<div>
+				<h4>This is suppose to be new component..Yay !!</h4>
+			</div>
+		);
+	}
+}
+
+export default MyNewComponent;
